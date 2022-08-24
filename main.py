bb24d4ca201f6bca164b51d0a7104655d36419df
laurebt <laurebt@users.noreply.github.com>
2022-08-24 13:19:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import streamlit as st
+
 def hello(name="Daisi"):
-  return f"Hello {name}!"
+    return f"Hello {name}!"
 
 def app():
-  st.write(hello())
+    name = st.text_input('Type your name', value = "Daisi user")
+    greeting = hello(name)
+    st.header(greeting)
 
 if __name__ == '__main__':
-  app()
+    app()
